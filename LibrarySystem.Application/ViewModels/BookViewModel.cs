108bbd64d4446008d0dfb44716da225026b68a76
joelcampos2014 <joelcampos147@gmail.com>
2024-06-05 20:05:18 -0300

    feat: config dos input/view models

--- LibrarySystem.Application/ViewModels/BookViewModel.cs
+++ LibrarySystem.Application/ViewModels/BookViewModel.cs
@@ -8,5 +8,15 @@ namespace LibrarySystem.Application.ViewModels
 {
     public class BookViewModel
     {
+        public BookViewModel(int id, string title, string author)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
     }
 }
